bc1fd18c7175fc45f14e9a8452ee0dca53973f53
guzmansb7 <guzman0007@gmail.com>
2020-09-16 06:00:20 -0700

    Updates

---
+++ precache-manifest.c376fabcfc856d832d5a6085740d33be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1edee5a26103c6a8db95859bfb4837a",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "eab8e03f658eb39ef0d9",
+    "url": "/ReactPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b60dfeccc9add2bcf692",
+    "url": "/ReactPortfolio/static/js/2.4b18b3d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPortfolio/static/js/2.4b18b3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab8e03f658eb39ef0d9",
+    "url": "/ReactPortfolio/static/js/main.90883737.chunk.js"
+  },
+  {
+    "revision": "0114f5eceb8464d5d956",
+    "url": "/ReactPortfolio/static/js/runtime-main.5b3ce1ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPortfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
